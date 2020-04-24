da4581c58c4590ab78a9ba28456be640e4db60fb
Henrique <hss@outlook.com.br>
2020-04-24 00:57:43 -0300

    C programs

---
+++ Programs-C/Fatorial.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	int fact, N;
+
+	scanf("%d", &N);
+
+	for(int i = 1, fact = 1; i <= N; i++)
+		fact *= i;
+	printf("\n %d! = %d", N, fact);
+    return 0;
+}
