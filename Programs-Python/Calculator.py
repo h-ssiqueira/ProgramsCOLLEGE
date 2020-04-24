1a3ae544aca32d73496c241799dc542b1ec1330d
Henrique <hss@outlook.com.br>
2020-04-24 00:56:53 -0300

    Python

---
+++ Programs-Python/Calculator.py
@@ -0,0 +1,4 @@
+print(eval(input("Insert your expression: ")))
+
+## Use + - * / // ** % < > == != <= >= 
+## You can put in the same line (number | operator | number) with/out different operations.
