da9f24eb131aca76cb876b99176d6cca1f772301
Henrique <hss@outlook.com.br>
2020-05-02 22:49:15 -0300

    C programs

--- Programs-C/Fatorial.c
+++ Programs-C/Fatorial.c
@@ -3,9 +3,10 @@
 
 int main(){
 	int fact, N, i;
-
-	scanf("%d", &N);
-
+	do{
+		printf("\tInsira o valor inteiro para calcular o fatorial: ");
+		scanf("%d", &N);
+	}while(N < 0);
 	for(i = 1, fact = 1; i <= N; i++)
 		fact *= i;
 	printf("\n %d! = %d", N, fact);
