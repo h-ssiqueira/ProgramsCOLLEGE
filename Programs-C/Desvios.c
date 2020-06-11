7ab960076058f2688542510c61ab971f50001527
Henrique <hss@outlook.com.br>
2020-06-11 14:41:03 -0300

    C programs

---
+++ Programs-C/Desvios.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+int main()
+{
+    int n,i;
+    float v1[11],v2[11],v3[11],som1=0,som2=0,som3=0,med1,med2,med3,s2,s,err;
+    printf("Digite um valor para n:");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        printf("\nDigite um valor:");
+        scanf("%f",&v1[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        som1+=v1[i];
+    }
+    med1=som1/n;
+    for(i=0;i<n;i++)
+    {
+        v2[i]=v1[i]-med1;
+        if(v2[i]<0)
+        {
+            v2[i]=v2[i]-2*v2[i];
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        som2+=v2[i];
+    }
+    med2=som2/n;
+    for(i=0;i<n;i++)
+    {
+        v3[i]=pow(v2[i],2);
+    }
+    for(i=0;i<n;i++)
+    {
+        som3+=v3[i];
+    }
+    med3=som3/n;
+    s2=som3/(n-1);
+    s=sqrt(s2);
+    err=s/sqrt(n);
+    system("cls");
+    printf("\n");
+    for(i=0;i<n+1;i++)
+    {
+        if(i!=n)
+        {
+            printf("   %d  | %.4f | %.4f | %.4f \n",i+1,v1[i],v2[i],v3[i]);
+        }
+        else
+        {
+            printf(" SOMA | %.4f | %.4f | %.4f \n\n",som1,som2,som3);
+        }
+    }
+    printf("Cálculo da Média: %.4f\n\n",med1);
+    printf("Cálculo do Desvio Absoluto Médio: %.4f\n\n",med2);
+    printf("Cálculo do Desvio-Quadratico Médio: %.4f\n\n",med3);
+    printf("Cálculo da Variância: %.4f\n\n",s2);
+    printf("Cálculo do Desvio Padrão: %.4f\n\n",s);
+    printf("Cálculo do Erro Padrão da Média: %.4f\n\n\n",err);
+    return 0;
+}
