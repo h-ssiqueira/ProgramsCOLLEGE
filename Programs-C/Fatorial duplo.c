6bf773cef53784cdee0ec804a21f02cd824c7161
Henrique <hss@outlook.com.br>
2020-07-04 19:42:57 -0300

    C programs

--- Programs-C/Fatorial duplo.c
+++ Programs-C/Fatorial duplo.c
@@ -2,18 +2,18 @@
 #include <stdlib.h>
 
 int fat(int n){
-if(n==1)return 1;
+if(n==1)
+    return 1;
 return (n*fat(n-2));
 }
 
-int main()
-{
+int main(){
     int n;
 
     do{
-    printf("\nInsira numero impar: ");
-    scanf("%d",&n);
-    system("cls");
+        printf("\nInsira numero impar: ");
+        scanf("%d",&n);
+        system("cls");
     }while(n%2==0);
 
     printf("Fatorial duplo de %d = %d",n,fat(n));
