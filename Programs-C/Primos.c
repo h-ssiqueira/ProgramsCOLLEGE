5caa072afd7f5363e4b4ac158dd3a339eddf5dd8
Henrique <hss@outlook.com.br>
2020-07-17 19:49:01 -0300

    C programs

--- Programs-C/Primos.c
+++ Programs-C/Primos.c
@@ -1,33 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <locale.h>
-int main()
-{
-    setlocale (LC_ALL,"portuguese");
+#include <math.h>
+
+int main(){
     int n, c, i;
 
+	/*
     do{
-    printf("\n Insira um número maior que 2: ");
-    scanf("%d", &n);
-     for(i=2, c=0;i<=n/2;i++){
-        if(n%i==0){
-        c++;
-        break;
-        }
-    }c==0?
-    printf("\n Número primo"):
+		printf("\n Insira um nÃºmero maior que 2: ");
+		scanf("%d", &n);
+		for(i=2, c=0;i<=n/2;i++){
+			if(n%i==0){
+				c++;
+				break;
+			}
+		}
+		c==0 ? printf("\n NÃºmero primo") : printf("\n NÃºmero nÃ£o primo");
+    }while(n > 2);
+	*/
 
-
-    printf("\n Número não primo");
-    }while(n>2);
-
-
-
-   // printf("\n Insira um número maior que 2: ");
-    //scanf("%d", &n);
-
-
-
+	do{
+		printf("\n Insira um nÃºmero maior que 2: ");
+		scanf("%d", &n);
+		for(i = 2, c = 0; i < sqrt(n); i++){
+			if(n % i == 0){
+				c++;
+				break;
+			}
+		}
+		c == 0 ? printf("\n NÃºmero primo") : printf("\n NÃºmero nÃ£o primo");
+	}while(n > 2);
 
     return 0;
 }
