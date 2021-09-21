255fcf49f57f2ce394e29ed16cdd1ec442080207
Henrique <hss@outlook.com.br>
2021-09-20 21:45:19 -0300

    TP lab06 p2

--- TP/lab06/banco.js
+++ TP/lab06/banco.js
@@ -3,22 +3,67 @@ class Banco{
         this._contas = [];
     }
     novaConta(tipo,nome,conta){
-
+        switch(tipo){
+            case 1:
+                this._contas.unshift(new Platinum(conta,nome));
+                break;
+            case 2:
+                this._contas.unshift(new Basica(conta,nome));
+                break;
+            case 3:
+                this._contas.unshift(new Estudante(conta,nome));
+                break;
+        }
     }
     listar(){
-
+        var info = "Conta\tTipo\t\tProprietário";
+        for(let i = 0; i < this._contas.length; i++)
+            info += this._contas[i].listar();
+        return info;
     }
     transferencia(contaDesc,valor,contaAcres){
-
+        var desconto = -1, credito = -1;
+        for(let i = 0; i < this._contas.length; i++){
+            if(this._contas[i].conta == contaDesc)
+                desconto = i;
+            else if(this._contas[i].conta == contaAcres)
+                credito = i;
+        }
+        if(desconto > -1 && credito > -1){
+            this._contas[desconto].transferencia(contaAcres,valor,false);
+            this._contas[credito].transferencia(contaDesc,valor,true);
+            return true;
+        }
+        return false;
     }
     saque(conta,valor){
-
+        for(let i = 0; i < this._contas.length; i++){
+            if(this._contas[i].conta == conta){
+                if(this._contas[i].sacar(valor))
+                    return true;
+                else
+                    return false;
+            }
+        }
     }
-    depósito(conta,valor){
-
+    deposito(conta,valor){
+        for(let i = 0; i < this._contas.length; i++){
+            if(this._contas[i].conta == conta)
+                this._contas[i].depositar(valor);
+        }
     }
     extrato(conta){
-
+        for(let i = 0; i < this._contas.length; i++){
+            if(this._contas[i].conta == conta)
+                return this._contas[i].extrato();
+        }
+        return false;
+    }
+    avancarmes(){
+        for(let i = 0; i < this._contas.length; i++){
+            if(this._contas[i] instanceof Basica || this._contas[i] instanceof Estudante)
+                this.contas[i].avancarMes();
+        }
     }
 }
 
@@ -54,34 +99,31 @@ class Platinum extends Conta{
         this._historico = [];
     }
     listar(){
-        return "\n" + this.conta() + "\tPlatinum\tR$" + this.nome();
+        return "\n" + this.conta + "\tPlatinum\t" + this.nome;
     }
     sacar(valor){
-        this.saldo(this.saldo() - valor);
-        this._historico.unshift("\nSaque\tR$" + valor + "\tR$" + this.saldo());
+        this.saldo(this.saldo - valor);
+        this._historico.unshift("\nSaque\tR$" + valor + "\tR$" + this.saldo);
         return true;
     }
     depositar(valor){
-        this.saldo(this.saldo() + valor);
-        this._historico.unshift("\nDepósito\tR$" + valor + "\tR$" + this.saldo());
-        return true;
+        this.saldo(this.saldo + valor);
+        this._historico.unshift("\nDepósito\tR$" + valor + "\tR$" + this.saldo);
     }
     extrato(){
-        var ext = "";
+        var ext = "Desc\tValor\tSaldo";
         for(var i = 0; i < this._historico.length; i++)
             ext += this._historico[i];
         return ext;
     }
     transferencia(conta,valor,recebida){
         if(!recebida){
-            this.saldo(this.saldo() - valor);
-            this._historico.unshift("\nTransferência (para " + conta + ")\tR$" + valor + "\tR$" + this.saldo());
-            return true;
+            this.saldo(this.saldo - valor);
+            this._historico.unshift("\nTransferência (para " + conta + ")\tR$" + valor + "\tR$" + this.saldo);
         }
         else{
-            this.saldo(this.saldo() + valor);
-            this._historico.unshift("\nTransferência (de " + conta + ")\tR$" + valor + "\tR$" + this.saldo());
-            return true;
+            this.saldo(this.saldo + valor);
+            this._historico.unshift("\nTransferência (de " + conta + ")\tR$" + valor + "\tR$" + this.saldo);
         }
     }
 }
@@ -106,43 +148,40 @@ class Basica extends Conta{
         return this._transferencias;
     }
     listar(){
-        return "\n" + this.conta() + "\tBásica\tR$" + this.nome();
+        return "\n" + this.conta + "\tBásica\t\t" + this.nome;
     }
     sacar(valor){
         if(valor <= 1000){
-            this.saldo(this.saldo() - valor);
-            this._historico.unshift("\nSaque\tR$" + valor + "\tR$" + this.saldo());
+            this.saldo(this.saldo - valor);
+            this._historico.unshift("\nSaque\tR$" + valor + "\tR$" + this.saldo);
             return true;
         }
         return false;
     }
     depositar(valor){
-        this.saldo(this.saldo() + valor);
-        this._historico.unshift("\nDepósito\tR$" + valor + "\tR$" + this.saldo());
-        return true;
+        this.saldo(this.saldo + valor);
+        this._historico.unshift("\nDepósito\tR$" + valor + "\tR$" + this.saldo);
     }
     operacaoExcedente(){
-        this.saldo(this.saldo() - .5);
-        this._historico.unshift("\nOp. excedente\tR$0,50\tR$" + this.saldo());
+        this.saldo(this.saldo - .5);
+        this._historico.unshift("\nOp. excedente\tR$0,50\tR$" + this.saldo);
     }
     extrato(){
-        this.extratos() < 3 ? this.extratos(this.extratos() + 1) : this.operacaoExcedente();
-        var ext = "";
+        this.extratos < 3 ? this.extratos(this.extratos + 1) : this.operacaoExcedente();
+        var ext = "Desc\tValor\tSaldo";
         for(var i = 0; i < this._historico.length; i++)
             ext += this._historico[i];
         return ext;
     }
     transferencia(conta,valor,recebida){
         if(!recebida){
-            this.transferencias() < 3 ? this.transferencias(this.transferencias() + 1) : this.operacaoExcedente();
-            this.saldo(this.saldo() - valor);
-            this._historico.unshift("\nTransferência (para " + conta + ")\tR$" + valor + "\tR$" + this.saldo());
-            return true;
+            this.transferencias < 3 ? this.transferencias(this.transferencias + 1) : this.operacaoExcedente();
+            this.saldo(this.saldo - valor);
+            this._historico.unshift("\nTransferência (para " + conta + ")\tR$" + valor + "\tR$" + this.saldo);
         }
         else{
-            this.saldo(this.saldo() + valor);
-            this._historico.unshift("\nTransferência (de " + conta + ")\tR$" + valor + "\tR$" + this.saldo());
-            return true;
+            this.saldo(this.saldo + valor);
+            this._historico.unshift("\nTransferência (de " + conta + ")\tR$" + valor + "\tR$" + this.saldo);
         }
     }
     avancarMes(){
@@ -159,28 +198,27 @@ class Estudante extends Conta{
         this._transferencias = false;
     }
     listar(){
-        return "\n" + this.conta() + "\tEstudante\tR$" + this.nome();
+        return "\n" + this.conta + "\tEstudante\t" + this.nome;
     }
     sacar(valor){
         if(valor <= 300){
-            this.saldo(this.saldo() - valor);
-            this._historico.unshift("\nSaque\tR$" + valor + "\tR$" + this.saldo());
+            this.saldo(this.saldo - valor);
+            this._historico.unshift("\nSaque\tR$" + valor + "\tR$" + this.saldo);
             return true;
         }
         return false;
     }
     depositar(valor){
-        this.saldo(this.saldo() + valor);
-        this._historico.unshift("\nDepósito\tR$" + valor + "\tR$" + this.saldo());
-        return true;
+        this.saldo(this.saldo + valor);
+        this._historico.unshift("\nDepósito\tR$" + valor + "\tR$" + this.saldo);
     }
     operacaoExcedente(){
-        this.saldo(this.saldo() - .5);
-        this._historico.unshift("\nOp. excedente\tR$0,50\tR$" + this.saldo());
+        this.saldo(this.saldo - .5);
+        this._historico.unshift("\nOp. excedente\tR$0,50\tR$" + this.saldo);
     }
     extrato(){
         !this._extratos ? this._extratos = true : this.operacaoExcedente();
-        var ext = "";
+        var ext = "Desc\tValor\tSaldo";
         for(var i = 0; i < this._historico.length; i++)
             ext += this._historico[i];
         return ext;
@@ -188,14 +226,12 @@ class Estudante extends Conta{
     transferencia(conta,valor,recebida){
         if(!recebida){
             this._transferencias ? this._transferencias = true : this.operacaoExcedente();
-            this.saldo(this.saldo() - valor);
-            this._historico.unshift("\nTransferência (para " + conta + ")\tR$" + valor + "\tR$" + this.saldo());
-            return true;
+            this.saldo(this.saldo - valor);
+            this._historico.unshift("\nTransferência (para " + conta + ")\tR$" + valor + "\tR$" + this.saldo);
         }
         else{
-            this.saldo(this.saldo() + valor);
-            this._historico.unshift("\nTransferência (de " + conta + ")\tR$" + valor + "\tR$" + this.saldo());
-            return true;
+            this.saldo(this.saldo + valor);
+            this._historico.unshift("\nTransferência (de " + conta + ")\tR$" + valor + "\tR$" + this.saldo);
         }
     }
     avancarMes(){
@@ -203,3 +239,6 @@ class Estudante extends Conta{
         this._transferencias = false;
     }
 }
+
+let banco = new Banco();
+let contaN = 1;
--- TP/lab06/interface.js
+++ TP/lab06/interface.js
@@ -1,27 +1,78 @@
 function novaConta(){
-
+    if(document.novo.nome.value !== "" && document.novo.tipo.checked !== false){
+        if(document.novo.tipo.value === "Premium")
+            banco.novaConta(1,document.novo.nome.value,contaN++);
+        else if(document.novo.tipo.value === "Básica")
+            banco.novaConta(2,document.novo.nome.value,contaN++);
+        else if(document.novo.tipo.value === "Estudante")
+            banco.novaConta(3,document.novo.nome.value,contaN++);
+        document.saida.bloco.value = "Conta criada.";
+    }
+    else
+        document.saida.bloco.value = "Informações ausentes para criação de nova conta.";
 }
 
 function listar(){
-    
+    document.saida.bloco.value = banco.listar();
 }
 
 function transferencia(){
-
+    if(document.transf.contaRem.value !== "" && document.transf.valor.value !== "" && document.transf.contaDest.value !== "" && document.transf.contaRem.value !== document.transf.contaDest.value){
+        if(banco.transferencia(document.transf.contaRem.value,document.transf.valor.value,document.transf.contaDest.value))
+            document.saida.bloco.value = "Transferência efetuada.";
+        else
+            document.saida.bloco.value = "Conta não encontrada.";
+    }
+    else
+        document.saida.bloco.value = "Informações ausentes para transferência de valores entre contas.";
 }
 
 function saque(){
-
+    if(document.saqueEdeposito.conta.value !== "" && document.saqueEdeposito.valor.value !== ""){
+        if(banco.saque(document.saqueEdeposito.conta.value,document.saqueEdeposito.valor.value))
+            document.saida.bloco.value = "Saque efetuado.";
+        else
+            document.saida.bloco.value = "Saque recusado: Limite atingido.";
+    }
+    else
+        document.saida.bloco.value = "Informações ausentes para sacar valores de uma conta.";
 }
 
 function deposito(){
-
+    if(document.saqueEdeposito.conta.value !== "" && document.saqueEdeposito.valor.value !== ""){
+        banco.deposito(document.saqueEdeposito.conta.value,document.saqueEdeposito.valor.value);
+        document.saida.bloco.value = "Deposito efetuado.";
+    }
+    else
+        document.saida.bloco.value = "Informações ausentes para depositar valores em uma conta.";
 }
 
 function extrato(){
-
+    var txt;
+    if(document.extrat.conta.value !== ""){
+        txt = banco.extrato(document.extrat.conta.value);
+        if(txt == false)
+            document.saida.bloco.value = "Conta não encontrada.";
+        else
+            document.saida.bloco.value = txt;
+    }
+    else
+        document.saida.bloco.value = "Número de conta ausente";
 }
 
 function limpar(){
+    document.novo.nome.value = "";
+    document.novo.tipo.checked = false;
+    document.transf.contaRem.value = "";
+    document.transf.valor.value = "";
+    document.transf.contaDest.value = "";
+    document.saqueEdeposito.conta.value = "";
+    document.saqueEdeposito.valor.value = "";
+    document.extrat.conta.value = "";
+    document.saida.bloco.value = "";
+}
 
+function avancar(){
+    banco.avancarmes();
+    document.saida.bloco.value = "Limite das contas zerado para o próximo mês.";
 }
